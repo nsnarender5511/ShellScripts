dbfadb4dec0b00fd6aa5005d86390e15d8f173ba
Narender <narender@Narenders-MacBook-Air.local>
2024-05-05 22:32:51 +0530

    Learning Shell 2

--- Learning_Shell_1.sh
+++ Learning_Shell_1.sh
@@ -1,4 +1,60 @@
 #!/bin/bash
 
-echo "Scripting is Fun"
+
+lineBreaker(){
+	for i in {1..20}
+	do
+        	echo -n  "---"
+	done
+	echo ""
+}
+
+
+
+
+
+variable1="Scripting is Fun!"
+variable2="Scripting is Fun"
+echo "$variable1"
+
+lineBreaker
+
+if [ "$variable1" = "$variable2" ]; then
+	echo "variable1 and variable2 are equal"
+else
+	echo "variable1 and variable2 are not equal"
+fi
+
+
+if [ "$variable1" != "${variable2}!" ]; then
+	echo "Both variables are equal after addition of !"
+fi
+
+
+lineBreaker
+
+
+file="test.txt"
+
+if [ -e "$file" ]; then
+	echo "File exists"
+else
+	echo "File Not Found"
+fi
+
+
+if [ -s "$file" ]; then
+	echo "File Not Empty"
+else 
+	echo "File Empty"
+fi
+
+
+if [ -w "$file" ]; then
+	echo "File is writable"
+else 
+	echo "File is Not Writable"
+fi
+
+lineBreaker
 
